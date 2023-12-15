09ce9ee2060cf15b818990cfd0d92a2db7fe9bdf
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2023-12-15 17:52:45 +0100

    hhhhhh

---
+++ OneDrive/Desktop/New Folder/Gestion_of_university/src/main/java/com/mycompany/gestion_of_university/Gestion_of_university.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestion_of_university;
+
+/**
+ *
+ * @author shop souf
+ */
+public class Gestion_of_university {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
